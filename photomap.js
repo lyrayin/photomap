84a9f40d8afb0403a7ed69f5948a23a209397ade
lyrayin <35706908+lyrayin@users.noreply.github.com>
2021-04-15 10:48:00 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -17,7 +17,7 @@ function createPhotoMap () {
   var csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTwpPjGRoQyZ04Kx2mWgVDsPoUTIT-qPN09vvcC6BCpGCwiIIjJysSjMfCIQESD-XOMoYrsWDDQolQg/pub?gid=0&single=true&output=csv';
   
   // create map object with center lat/lon and zoom level
-  var map = L.map('map').setView([38.7, -111.7], 8);
+  var map = L.map('map').setView([38.7, -110.9], 8);
   
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
   var basemap = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
